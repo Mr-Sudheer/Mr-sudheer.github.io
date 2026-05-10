af292f007e3f6d0f477364a1cdd98c7a1e693163
Chillakuru Sudheer <senorsudheer@gmail.com>
2026-05-10 19:05:05 +0530

    Add linux platform support

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778419353',resources: [
+    cacheName: 'chirpy-1778419917',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
