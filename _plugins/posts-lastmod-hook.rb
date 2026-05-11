5c09d595e36d9bf522d9c91bfabce2f1d399deb3
Chillakuru Sudheer <senorsudheer@gmail.com>
2026-05-11 12:59:11 +0530

    Added corridor walkthrough

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -4,11 +4,15 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  begin
+    commit_num = `git rev-list --count HEAD #{ post.path }`
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    if commit_num.to_i > 1
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso #{ post.path }`
+      post.data['last_modified_at'] = lastmod_date
+    end
+  rescue
+    # Skip if git is not available
   end
 
 end
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1778421197',resources: [
+    cacheName: 'chirpy-1778483218',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
